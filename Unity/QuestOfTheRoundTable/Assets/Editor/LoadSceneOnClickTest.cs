8a422f5f3293acbcf5107007beb77eb362a5489e
Michael <michaelkuang007@gmail.com>
2018-01-24 08:55:18 -0500

    wip

---
+++ Unity/QuestOfTheRoundTable/Assets/Editor/LoadSceneOnClickTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LoadSceneOnClickTest : IPrebuildSetup {
+    [SetUp]
+    public void Setup()
+    {
+        LoadSceneOnClick.print("Setting Up");
+    }
+	[Test]
+	public void LoadSceneOnClickTestSimplePasses() {
+        LoadSceneOnClick.print("Testing");
+        
+        Assert.AreEqual(true, true);
+	}
+
+/*	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator LoadSceneOnClickTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+    */
+}
---
+++ Unity/QuestOfTheRoundTable/Assets/Scripts/Objects/StoryCard.cs
@@ -0,0 +1,21 @@
+﻿namespace Card
+{
+    public class StoryCard : Card
+    {
+        public enum StoryType
+        {
+            Quest, Event, Tournament
+        }
+        protected StoryType Type { get; set; }
+       // protected string Description { get; set; }
+        public StoryCard()
+        {
+
+        }
+        public StoryCard(string name, StoryType type) : base(name)
+        {
+            Type = type;
+        }
+    }
+
+}
