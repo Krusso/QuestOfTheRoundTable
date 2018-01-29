1711a649b8ab4e099f4eef48ecbc486c7d32e58c
Michael <michaelkuang007@gmail.com>
2018-01-28 20:10:12 -0500

    completed the start game transition and made some prefabs

---
+++ Unity/QuestOfTheRoundTable/Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+
+    private const int gameScene = 1;
+    public GameObject[] playerObjects;
+    public Player[] players;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("I am alive");
+	}
+
+    public void NewGame(int numPlayers)
+    {
+        
+
+        players = new Player[numPlayers];
+        SceneManager.LoadScene(gameScene);
+    }
+}
--- Unity/QuestOfTheRoundTable/Assets/Scripts/StartGameOption.cs
+++ Unity/QuestOfTheRoundTable/Assets/Scripts/StartGameOption.cs
@@ -10,25 +10,31 @@ public class StartGameOption : MonoBehaviour {
     private GameObject NumPlayersField;
     private Text playerfieldText;
     private const int gameScene = 1;
+    public Game game;
     
     private void Awake()
     {
+        DontDestroyOnLoad(game);
     }
     void Start()
     {
         NumPlayersField = GameObject.Find("NumPlayersField");
-       
     }
 	
 	// Update is called once per frame
 	void Update ()
-    {
-        Debug.Log("playerfieldText: " + playerfieldText.text);
-        
+    {      
     }
     public void StartGame()
     {
-
+        GameObject playersfieldtext = GameObject.Find("/Canvas/PlayerOptionPanel/NumPlayersField/Text");
+        Debug.Log("playersfieldtext: " + playersfieldtext.ToString());
+        Text txt = playersfieldtext.GetComponent<Text>();
+        Debug.Log("txt: " + txt.text);
+        game = new Game();
+        game.NewGame(int.Parse(txt.text));
 
     }
+
+    
 }
