12260f228c80606bde56c261cc29b9a7ad804820
Neha-Rao <nehakrao.1997@gmail.com>
2018-02-01 17:05:14 -0500

    deleted round

--- JavaFXQTRT/src/src/sequence/QuestCreator.java
+++ JavaFXQTRT/src/src/sequence/QuestCreator.java
@@ -10,32 +10,37 @@ import java.util.regex.Pattern;
 
 public class QuestCreator {
 
+	int stages;
 	Player sponsor;
 	List<List<Card>> quest;
 
-	public QuestCreator(Player sponsor) {
+	public QuestCreator(int stages, Player sponsor) {
+		this.stages = stages;
 		quest = new ArrayList<List<Card>>();
 	}
 	
-	public void setUpQuest(LinkedBlockingQueue<String> actions) {
+	public List<List<Card>> setUpQuest(LinkedBlockingQueue<String> actions) {
 		String string;
-		try {
-			string = actions.take();
-			Pattern p = Pattern.compile("quest stage picked: (^\\d$) cards: (.*)");
-			Matcher m = p.matcher(string);
-			m.find();
-			int stage = Integer.parseInt(m.group(1));
-			String[] cards = m.group(2).split(",");
-			List<Card> cardlist = new ArrayList<Card>();
-			for(int i=0; i<cards.length; i++) {
-				Card card = (Card) Class.forName(cards[i]).newInstance(); // :^)
-				cardlist.add(card);
+		while(quest.size()<stages) {
+			try {
+				string = actions.take();
+				Pattern p = Pattern.compile("quest stage picked: (^\\d$) cards: (.*)");
+				Matcher m = p.matcher(string);
+				m.find();
+				int stage = Integer.parseInt(m.group(1));
+				String[] cards = m.group(2).split(",");
+				List<Card> cardlist = new ArrayList<Card>();
+				for(int i=0; i<cards.length; i++) {
+					Card card = (Card) Class.forName(cards[i]).newInstance(); // :^)
+					cardlist.add(card);
+				}
+				quest.add(stage, cardlist);
+				System.out.println("Quest Stage recieved: " + string);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			quest.add(stage, cardlist);
-			System.out.println("Quest Stage recieved: " + string);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+		return quest;
 	}
 }
