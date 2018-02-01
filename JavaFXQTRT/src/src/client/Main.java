ebcd1db9e5eb96f16a92753a61090d85b14a5223
Michael Kuang <michaelkuang97@gmail.com>
2018-02-01 14:50:16 -0500

    Add client file for java

--- JavaFXQTRT/src/src/client/Main.java
+++ JavaFXQTRT/src/src/client/Main.java
@@ -8,35 +8,21 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
-public class test extends Application {
+public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = new AnchorPane();
 			root = FXMLLoader.load(getClass().getResource("TitleScreen.fxml"));
-			
-			
 			Scene scene = new Scene(root);
-			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
-//
-//	    	FXMLLoader fxml = new FXMLLoader();
-//	    	fxml.setLocation(getClass().getResource("TitleScreen.fxml"));
-//	   
-//	    	Parent root = fxml.load();
-//
-//	    	Scene scene = new Scene(root);
-//	    	stage.setTitle("Quest of the Round Table");
-//	    	stage.setScene(scene);
-//	    	stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
