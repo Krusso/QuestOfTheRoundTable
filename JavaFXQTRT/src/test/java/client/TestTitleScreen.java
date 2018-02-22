4f442451e884f52b2e828a4d7a8f230fa4d6333e
Michael <michaelkuang007@gmail.com>
2018-02-22 17:39:31 -0500

    added threading rule

---
+++ JavaFXQTRT/src/test/java/client/TestTitleScreen.java
@@ -0,0 +1,25 @@
+package client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import src.client.Main;
+import src.socket.Server;
+
+public class TestTitleScreen {
+	
+	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+
+	
+	@Test
+	public void testStartGame() {
+		
+		
+		
+		System.out.println("Hello");
+		assertTrue(true);
+	}
+
+}
