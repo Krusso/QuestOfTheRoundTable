80a0ca2cd51d58f899b6fca69342c645a7f0e814
Michael Kuang <michaelkuang97@gmail.com>
2018-02-26 11:35:43 -0500

    fixed some logging

--- JavaFXQTRT/src/main/java/src/client/AudioPlayer.java
+++ JavaFXQTRT/src/main/java/src/client/AudioPlayer.java
@@ -10,12 +10,15 @@ import javafx.util.Duration;
 public class AudioPlayer {
 
 	final static Logger logger = LogManager.getLogger(AudioPlayer.class);
+	String name;
 	Media sound;
 	MediaPlayer mediaPlayer;
 	public AudioPlayer(String musicFile) {
+		name = musicFile;
 		setNewMusic(musicFile, true);
 	}
 	public AudioPlayer(String musicFile, boolean isLoop) {
+		name = musicFile;
 		setNewMusic(musicFile, isLoop);
 	}
 	
@@ -36,15 +39,15 @@ public class AudioPlayer {
 	public void play() {
 		stop();
 		mediaPlayer.play();
-		logger.info("Playing audio: " + sound.toString());
+		logger.info("Playing audio: " + name);
 	}
 	public void stop() {
 		mediaPlayer.stop();
-		logger.info("Stopped audio: " + sound.toString());
+		logger.info("Stopped audio to reset startTime: " + name);
 	}
 	public void setVolume(double value) {
 		mediaPlayer.setVolume(value);
-		logger.info("Set audio volume  [" + mediaPlayer.toString() + "] to:" +  mediaPlayer.getVolume() );
+		logger.info("Set audio volume  [" + sound + "] to:" +  mediaPlayer.getVolume() );
 	}
 	
 
