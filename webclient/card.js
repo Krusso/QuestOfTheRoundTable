b1682a8dcd9a8265de04f165d52911d52eff8f84
Michael Kuang <michaelkuang97@gmail.com>
2018-03-16 19:15:46 -0400

    checkpoint

--- webclient/card.js
+++ webclient/card.js
@@ -2,6 +2,9 @@ var res = "resources/";
 var app = angular.module('gameApp', ['ngDragDrop']);
 
 app.controller('gameController', function ($scope) {
+    $scope.calls = 0;
+    $scope.currentDrag;
+    $scope.cardId=0;
     $scope.game = {
         "hand": [],
         "field": []
@@ -12,14 +15,32 @@ app.controller('gameController', function ($scope) {
         console.log($scope.game);
         card = {
             name: n,
-            id: id,
+            id: $scope.cardId++,
             draggable: true,
         };
+        
         $scope.game.hand.push(card);
     };
+    
+    $scope.startDrag = function(event, card){
+        console.log(event);
+        console.log("asdf")
+        console.log(card);
+//        console.log(event.currentTarget);
+        $scope.calls = $scope.calls + 1;
+        $scope.currentDrag = card;
+    };  
+    
+    $scope.onDrop = function(event){
+        
+        console.log(event);
+    }
+    
+    $scope.findCardById = function(id){
+        
+    }
 
 });
-
 
 //Example templating:
 //<div card-img="<url>"></div>
