72236b249a5db4f30aea61e335dae5235a4de612
Michael <michaelkuang007@gmail.com>
2018-03-16 21:57:56 -0400

    feature drag and drop divs using angularjs

--- webclient/card.js
+++ webclient/card.js
@@ -2,42 +2,152 @@ var res = "resources/";
 var app = angular.module('gameApp', ['ngDragDrop']);
 
 app.controller('gameController', function ($scope) {
-    $scope.calls = 0;
-    $scope.currentDrag;
-    $scope.cardId=0;
-    $scope.game = {
-        "hand": [],
-        "field": []
+
+    /*=========================================   *
+     *            Controller Variables            *
+     *=========================================== */
+
+
+    $scope.currentDrag; //card id of the currently dragged card, null otherwise.
+    $scope.cardId = 0;
+    $scope.zones = {
+        "handZone": [],
+        "faceDownZone": [],
+        "faceUpZone": [],
+        "stage1Zone": [],
+        "stage2Zone": [],
+        "stage3Zone": [],
+        "stage4Zone": [],
+        "stage5Zone": []
     };
-    $scope.hand = [];
     $scope.playerId = 0;
     $scope.addCard = function (n, id) {
-        console.log($scope.game);
         card = {
             name: n,
-            id: $scope.cardId++,
+            id: ($scope.cardId++).toString(),
             draggable: true,
         };
-        
-        $scope.game.hand.push(card);
+
+        $scope.zones.handZone.push(card);
     };
-    
-    $scope.startDrag = function(event, card){
-        console.log(event);
-        console.log("asdf")
+
+    /*=========================================   *
+     *             Dragging Functions             *
+     *=========================================== */
+
+    $scope.startDrag = function (event) {
+        var cardId = event.currentTarget.id;
+        console.log("Start drag on card id - " + cardId)
+        $scope.currentDrag = cardId;
+    };
+
+    //the event.target returns the div element of the dropped over target. Use event.target.id to get the id of the div.
+    $scope.onDrop = function (event) {
+        console.log("Trying to drop into zone - " + event.target.id);
+        //array of where the currently dragged card originated from
+        var originalZone = $scope.findZoneWithCardId($scope.currentDrag);
+        var targetZone = $scope.getZone(event.target.id);
+        var card = originalZone.map(function (e) {
+            console.log(e.id);
+            console.log($scope.currentDrag);
+            if (e.id == $scope.currentDrag) {
+                console.log(e);
+                return e;
+            }
+        })[0];
         console.log(card);
-//        console.log(event.currentTarget);
-        $scope.calls = $scope.calls + 1;
-        $scope.currentDrag = card;
-    };  
-    
-    $scope.onDrop = function(event){
-        
-        console.log(event);
+        console.log("Copying card " + card.name + " of id " + card.id + " to target zone");
+        targetZone.push(card);
+        console.log("Removing card from original zone");
+        var pos = $scope.getIndexOfCardInZone(originalZone, $scope.currentDrag);
+        originalZone.splice(pos, 1);
+        console.log("position of card to remove: " + pos);
     }
-    
-    $scope.findCardById = function(id){
-        
+
+    /*=========================================  *
+     *              Helper functions              *
+     *=========================================== */
+    /*
+        Parameters: zone, an array that contains card objects
+                    id, a string of the card
+        return: index of the card in the zone array
+    */
+    $scope.getIndexOfCardInZone = function (zone, id) {
+        return zone.map(function (e) {
+            return e.id
+        }).indexOf(id);
+    }
+
+    /*
+        Parameters: name, the name of the zone in $scope.zones
+        return: the zone array corresponding the the passed in name
+    */
+    $scope.getZone = function (name) {
+        var zone;
+        switch (name) {
+            case "handZone":
+                zone = $scope.zones.handZone;
+                break;
+            case "faceDownZone":
+                zone = $scope.zones.faceDownZone;
+                break;
+            case "faceUpZone":
+                zone = $scope.zones.faceUpZone;
+                break;
+            case "stage1Zone":
+                zone = $scope.zones.stage1Zone;
+                break;
+            case "stage2Zone":
+                zone = $scope.zones.stage2Zone;
+                break;
+            case "stage3Zone":
+                zone = $scope.zones.stage3Zone;
+                break;
+            case "stage4Zone":
+                zone = $scope.zones.stage4Zone;
+                break;
+            case "stage5Zone":
+                zone = $scope.zones.stage5Zone;
+                break;
+        }
+        if (zone != null) {
+            console.log("returning zone: " + name);
+        } else {
+            console.log("Could not find zone with name - " + name);
+        }
+        return zone;
+    }
+
+    /*
+        Parameters: id, the id of the card
+        return: the zone array that contains this card id. Null if it does not exists
+    */
+    $scope.findZoneWithCardId = function (id) {
+        console.log("Trying to find card with id - " + id);
+        if ($scope.zoneContainCard(id, $scope.zones.handZone)) {
+            console.log("Card id - (" + id + ") is in handZone");
+            return $scope.zones.handZone;
+        }
+        if ($scope.zoneContainCard(id, $scope.zones.faceDownZone)) {
+            console.log("Card id - ()" + id + ") is in faceDownZone");
+            return $scope.zones.faceDownZone;
+        }
+        console.log("Card does not exists in any zones");
+        return null;
+    }
+
+    /*
+        Parameters: id, the card id
+                    cardArray, an array that contains cards
+        return: true if the cardArray contains a card with id. false otherwise
+    */
+    $scope.zoneContainCard = function (id, cardArray) {
+        for (var i = 0; i < cardArray.length; i++) {
+            if (cardArray[i].id == i.toString()) {
+                return true;
+            }
+        }
+        return false
     }
 
 });
